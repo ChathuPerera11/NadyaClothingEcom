645cd3f1838360e87bdc906792a282a0335efb5a
cperera11 <chathuperera11@gmail.com>
2022-04-06 22:43:04 -0500

    Verified Add and Retrieve methods for Product

--- src/main/java/com/chathupererajay/nadyafashion/dto/ProductDTO.java
+++ src/main/java/com/chathupererajay/nadyafashion/dto/ProductDTO.java
@@ -1,4 +1,3 @@
-
 package com.chathupererajay.nadyafashion.dto;
 
 import lombok.Data;
@@ -14,6 +13,67 @@ public class ProductDTO {
     private String name;
     private double price;
     private String description;
-    private String imageUrl;
+    private String imageName;
     private int categoryId;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(int id, String name, double price, String description, String imageName, int categoryId) {
+        setId(id);
+        setName(name);
+        setPrice(price);
+        setDescription(description);
+        setImageName(imageName);
+        setCategory(categoryId);
+    }
+
+    public final void setId(int id) {
+        this.id = id;
+    }
+
+    public final void setName(String name) {
+        this.name = name;
+    }
+
+    public final void setPrice(double price) {
+        this.price = price;
+    }
+
+    public final void setDescription(String description) {
+        this.description = description;
+    }
+
+    public final void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public final void setCategory(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public int getCategory() {
+        return categoryId;
+    }
+
 }
